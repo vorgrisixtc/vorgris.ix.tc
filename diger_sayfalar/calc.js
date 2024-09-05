49a57304c9867bf224973e9ba1b2458dccbf3d28
vorgrisixtc <166867713+vorgrisixtc@users.noreply.github.com>
2024-09-05 15:22:51 +0300

    hesap makinesi ve saat güncellendi

--- diger_sayfalar/calc.js
+++ diger_sayfalar/calc.js
@@ -1,136 +1,155 @@
-numbers = {
-    'zero': 0,
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9
+//Instanciate variables
+var historic = '';
+var current = '0';
+var answer = 0;
+var remind;
+
+//Handle clicks and keyboard events
+$(document).ready(function() {
+	$(document).keydown(function(evt) {
+		var code = evt.key;
+		if ($(':focus').is('button')) {
+			evt.preventDefault();
+		}
+		$('button[value="' + code + '"]').addClass('down');
+		setTimeout(function() {
+			$('button[value="' + code + '"]').removeClass('down');
+		}, 300);
+		handleValue(code);
+	});
+	$('button').click(function(evt) {
+		var code = $(this).attr("value");
+		handleValue(code);
+	});
+});
+
+function handleValue(entry) {
+	//Hide error notification if it appeared earlier
+	$('header').slideUp();
+
+	//Store result from past calculation in historic
+	if (remind) {
+		historic = current;
+		remind = false;
+	}
+	//Clear all entries
+	if (entry === 'clearall') {
+		resetCalc(true, true);
+	}
+	//Clear the current entry
+	if (entry === 'clearentry') {
+		resetCalc(true, false);
+		answer = 0;
+	}
+	//Handle deletion of characters
+	if (entry === 'Backspace') {
+		if (current !== '' && current !== '0' && answer === 0) {
+			current = current.slice(0, -1);
+			historic = historic.slice(0, -1);
+			addText();
+		} else {
+			displayError("Silinecek herhangi birşey kalmadı.");
+		}
+	}
+
+	//Handle operators
+	if (entry === '/' || entry === '*' || entry === '+' || entry === '-') {
+		if (current === '0' || current === '') {
+			displayError("Hiçbir şey üzerinde çalışamazsınız.");
+		} else {
+			current = entry;
+			historic += entry;
+			addText();
+			answer = 0;
+			current = '';
+		}
+	}
+
+	//Hande Enter/Validation
+	if (entry === 'Enter') {
+		calculate();
+		remind = true;
+	}
+
+	//Handle Decimals
+	if (entry === '.') {
+		if (current.indexOf('.') === -1) {
+			if (answer !== 0) {
+				resetCalc(true, true);
+			}
+			if (current === '') {
+				current = '0' + entry;
+				historic += current;
+			} else {
+				if (historic === '')
+					historic += '0' + entry;
+				else
+					historic += entry;
+				current += entry;
+			}
+			addText();
+		} else {
+			displayError("Bir sayıda birden fazla ondalık sayı olamaz.");
+		}
+	}
+
+	//Handle numbers
+	if (!isNaN(entry)) {
+		if (answer !== 0) {
+			resetCalc(true, true);
+		}
+		if (current === '0' && historic === '') {
+			current = entry;
+			historic = entry;
+		} else {
+			if (current === '0') {
+				current = entry;
+			} else {
+				current += entry;
+			}
+			historic += entry;
+		}
+		addText();
+		answer = 0;
+	}
+	console.log('current', current, 'historic', historic, 'answer', answer);
 }
 
-var finalResult = 0;
-var operator ='';
-var activeOperator = false;
-
-$('#ac').click(() => {
-    $('.result').text('0');
-})
-
-$('#sign').click(() => {
-    if (firstChar() === '-'){
-        result = $('.result').text()
-        sbstr = result.substring(1, result.length);
-        $('.result').text(sbstr);
-    } else if (!emptyResult()) {
-        prepend('-');
-    }
-})
-
-$('#percentage').click(() => {
-    if (!emptyResult()){
-        percentage = parseFloat($('.result').text())/100;
-        $('.result').text(percentage);
-    }
-})
-
-$('.operator').click(e => {
-    id = e.target.id;
-
-    if (id === 'equal'){
-        calculate();
-        $('.result').text(finalResult);
-        operator = '';
-        activeOperator = false;
-    } else {
-        operator = id;
-        activeOperator = true;
-    }
-})
-
-$('.number').click(e => {
-    if (firstChar() === '0' && !pointIncluded()) {
-        $('.result').text('');
-    }
-
-    id = e.target.id;
-    num = numbers[id];
-
-    if (firstChar() === '0'){
-        if (secoundChar() === '.'){
-            append(num)
-        }
-    }
-
-    if (activeOperator){
-        finalResult = parseFloat($('.result').text());
-        $('.result').text('');
-        activeOperator = false;
-    }
-
-    if (firstChar() === '0'){
-        if (hasChar('.')) { append(num); }
-    } else { append(num); }
-})
-
-$('#point').click(() => {
-    if (emptyResult()){
-        append('0.')
-    } else if (!emptyResult() && !pointIncluded()) { 
-        append('.'); 
-    } 
-})
-
-const calculate = () => {
-    actResult = parseFloat($('.result').text());
-    switch (operator){
-        case 'addition':
-            finalResult += actResult;
-            break;
-        case 'subtraction':
-            finalResult -= actResult;
-            break;
-        case 'multiplication':
-            finalResult *= actResult;
-            break;
-        case 'division':
-            finalResult /= actResult;
-            break;
-        default:
-            break;
-    }
+//Add text to the calculator
+function addText() {
+	$('#current').html(current);
+	$('#history').html(historic);
 }
 
-const emptyResult = () => {
-    return $('.result').text() === '';
+//Reset the calculator partially or not
+function resetCalc(current_res, history_res) {
+	if (current_res) {
+		current = '0';
+		$('#current').html(current);
+	}
+	if (history_res) {
+		historic = '';
+		$('#history').html(historic);
+		answer = 0;
+	} else {
+		var test = historic.split(/([\/\*\+\-])/g);
+		if (!isNaN(test[test.length - 1])) {
+			test.pop();
+			historic = test.join('');
+		}
+		$('#history').html(historic);
+	}
 }
 
-const hasChar = char => {
-    result = $('.result').text();
-    return result.index0f(char) !== -1;
+//Calculate the result
+function calculate() {
+	answer = eval(historic);
+	current = answer.toString();
+	addText();
 }
 
-const firstChar = () => {
-    return $('.result').text().charAt(0);
-}
-
-const secoundChar = () => {
-    return $('.result').text().charAt(1);
-}
-
-const pointIncluded = () => {
-    result = $('.result').text();
-    return result.includes('.');
-}
-
-const append = txt => {
-    result = $('.result').text();
-    $('.result').text(result + txt)
-}
-
-const prepend = sign => {
-    result = $('.result').text();
-    $('.result').text(sign + result);
+//Display errors
+function displayError(string) {
+	$('header').slideDown();
+	$('.error').text(string);
 }
